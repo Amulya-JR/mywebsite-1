a875f0ab017b20c42175207a1a078910c3ad028f
Amulya-JR <73379880+Amulya-JR@users.noreply.github.com>
2020-10-24 13:11:30 +0530

    Add files via upload

---
+++ Acquisitorstore/readme.txt
@@ -0,0 +1,22 @@
+WELCOME TO ACQUISITOR ONLINE STORE WEBSITE
+met all minimum requirements 
+"Please check my login validation,Wishlist,Star rating,map Zoom,dropdown menu,Self made Banner with button on it and footer page these are my additions""
+please refer to my VIDEO link in google form sir
+
+
+Home page
+about page
+2 categories page
+contact us page
+THESE ARE MY HIGLIGHTED POINTS OTHER THAN MIN REQUIREMENTS
+1)Home page: AUTOMATIC SLIDER ,logo,title,nav menu (Dropdown),Search,login and signup MODAL POPUPS and verified through JAVASCRIPT,listed items,SELF MADE BANNER  WITH BUTTON.
+2)footer:logo,address,questions
+3)Categories page:SLIDER,listed items,Description
+Description:STAR RATING,WISHLIST  click on which makes it red
+4)about us:Team members,contact buttons for members
+5)Contact:Contact form JAVASCRIPT VALIDATION,ENLARGING MAPS,Social icons button
+my screen resolution is 1920X1080 
+it is working fine in my laptop 
+if it is dislocated in your laptop sir,  please check the LINK of Recording video of my project submitted in google form
+
+                      THANK YOU
---
+++ Acquisitorstore/script.js
@@ -0,0 +1,61 @@
+function openlogin() {
+  document.getElementById('id01').style.display = 'block';
+}
+function opensignup() {
+  document.getElementById('id02').style.display = 'block';
+}
+
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "" && password == ""){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
